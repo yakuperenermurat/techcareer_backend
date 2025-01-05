1db68a62a20b069096596a4bf29034b352e6ce2e
yakuperenermurat <yakuperenermurat@gmail.com>
2025-01-05 19:07:27 +0300

    project create

--- src/main/java/com/yakuperenermurat/techcareer_2005_backend_1/Techcareer2005Backend1Application.java
+++ src/main/java/com/yakuperenermurat/techcareer_2005_backend_1/Techcareer2005Backend1Application.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class Techcareer2005Backend1Application {
 
-	public static void main(String[] args) {
-		SpringApplication.run(Techcareer2005Backend1Application.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(Techcareer2005Backend1Application.class, args);
+    }
 
 }
--- src/test/java/com/yakuperenermurat/techcareer_2005_backend_1/Techcareer2005Backend1ApplicationTests.java
+++ src/test/java/com/yakuperenermurat/techcareer_2005_backend_1/Techcareer2005Backend1ApplicationTests.java
@@ -6,8 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class Techcareer2005Backend1ApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
 
 }
