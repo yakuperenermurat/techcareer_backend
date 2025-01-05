c2d21ede6f05c9d6e69f900ee6900f971ca94311
yakuperenermurat <yakuperenermurat@gmail.com>
2025-01-05 19:07:03 +0300

    first commit

---
+++ src/main/java/com/yakuperenermurat/techcareer_2005_backend_1/Techcareer2005Backend1Application.java
@@ -0,0 +1,13 @@
+package com.yakuperenermurat.techcareer_2005_backend_1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Techcareer2005Backend1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Techcareer2005Backend1Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/yakuperenermurat/techcareer_2005_backend_1/Techcareer2005Backend1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yakuperenermurat.techcareer_2005_backend_1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Techcareer2005Backend1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
